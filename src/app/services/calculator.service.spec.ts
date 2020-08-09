d030802fd7f4f046113c22f1d09f1a5cdf35e1c2
rickliveshere <rick.read@corp.sigmauk>
2020-08-09 19:21:02 +0100

    Add failing tests for decimals :)

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -9,32 +9,132 @@ describe('CalculatorService', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     expect(service).toBeTruthy();
   });
+});
 
-  it('can add 2 positive numbers together', () => {
+describe('CalculatorService - Addition', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('can add 2 positive integers together', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.add(10, 15);
     expect(result).toBe(25);
   });
 
-  it('can subtract 2 positive numbers', () => {
+  it('can add positive and negative integers together', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.add(-10, 15);
+    expect(result).toBe(5);
+  });
+
+  it('can add 2 negative integers together', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.add(-10, -15);
+    expect(result).toBe(-25);
+  });
+
+  it('can add 2 positive decimals together', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.add(0.5, 0.2);
+    expect(result).toBe(0.7);
+  });
+
+  it('can add positive and negative decimals together', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.add(-0.5, 0.2);
+    expect(result).toBe(-0.3);
+  });
+
+  it('can add 2 negative decimals together', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.add(-0.5, -0.5);
+    expect(result).toBe(-1);
+  });
+});
+
+describe('CalculatorService - Subtraction', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('can subtract 2 positive integers', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.subtract(15, 10);
     expect(result).toBe(5);
   });
 
-  it('can subtract a larger number from a smaler number and receive a negative result', () => {
+  it('can subtract a larger integers from a smaller integers and receive a negative result', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.subtract(10, 15);
     expect(result).toBe(-5);
   });
 
-  it('can multiply 2 positive numbers', () => {
+  it('can subtract 2 negative integers', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.subtract(-10, -15);
+    expect(result).toBe(5);
+  });
+
+  it('can subtract 2 positive decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.subtract(0.7, 0.2);
+    expect(result).toBe(0.5);
+  });
+
+  it('can subtract a larger decimal from a smaller decimal and receive a negative result', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.subtract(0.5, 0.7);
+    expect(result).toBe(-0.2);
+  });
+
+  it('can subtract 2 negative decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.subtract(-0.5, -1.5);
+    expect(result).toBe(2);
+  });
+});
+
+describe('CalculatorService - Multiplication', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('can multiply 2 positive integers', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.multiply(10, 20);
     expect(result).toBe(200);
   });
 
-  it('can divide 2 positive numbers', () => {
+  it('can multiply 2 negative integers', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.multiply(-5, -5);
+    expect(result).toBe(25);
+  });
+
+  it('can multiply a positive and negative integer', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.multiply(-10, 20);
+    expect(result).toBe(-200);
+  });
+
+  it('can multiply 2 positive decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.multiply(10.5, 20.5);
+    expect(result).toBe(215.25);
+  });
+
+  it('can multiply 2 negative decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.multiply(-5.1, -5.4);
+    expect(result).toBe(27.54);
+  });
+
+  it('can multiply a positive and negative decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.multiply(10.1, -20.2);
+    expect(result).toBe(-204.02);
+  });
+});
+
+describe('CalculatorService - Division', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('can divide 2 positive integers', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.divide(10, 5);
     expect(result).toBe(2);
@@ -46,38 +146,26 @@ describe('CalculatorService', () => {
     expect(result).toBe(0.5);
   });
 
-  it('can add 2 negative numbers together', () => {
+  it('cannot divide by 0', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
-    let result = service.add(-10, -15);
-    expect(result).toBe(-25);
+    expect(function() { service.divide(10, 0); }).toThrow(new Error("Second number cannot be zero"));
   });
 
-  it('can add positive and negative numbers together', () => {
-    const service: CalculatorService = TestBed.get(CalculatorService);
-    let result = service.add(-10, 15);
-    expect(result).toBe(5);
-  });
-
-  it('can subtract 2 negative numbers', () => {
-    const service: CalculatorService = TestBed.get(CalculatorService);
-    let result = service.subtract(-10, -15);
-    expect(result).toBe(5);
-  });
-
-  it('can multiply 2 negative numbers', () => {
-    const service: CalculatorService = TestBed.get(CalculatorService);
-    let result = service.multiply(-5, -5);
-    expect(result).toBe(25);
-  });
-
-  it('can divide 2 negative numbers', () => {
+  it('can divide 2 negative integers', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
     let result = service.divide(-10, -5);
     expect(result).toBe(2);
   });
 
-  it('cannot divide by 0', () => {
+  it('can divide 2 positive decimals', () => {
     const service: CalculatorService = TestBed.get(CalculatorService);
-    expect(function() { service.divide(10, 0); }).toThrow(new Error("Second number cannot be zero"));
+    let result = service.divide(10.0, 5.0);
+    expect(result).toBe(2);
+  });
+
+  it('can divide 2 negative decimals', () => {
+    const service: CalculatorService = TestBed.get(CalculatorService);
+    let result = service.divide(-10.0, -5.0);
+    expect(result).toBe(2);
   });
 });
